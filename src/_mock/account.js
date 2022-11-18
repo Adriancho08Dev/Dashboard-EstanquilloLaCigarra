762c58e4c581c6d8ff177f4c06d5431fd3367c85
Adriancho08Dev <naadrian495@gmail.com>
2022-11-18 16:09:54 -0500

    Se agrega estructura inicial del proyecto.

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Adrian Correa',
+  email: 'adrian.correa@utp.edu.co',
+  photoURL: '/assets/images/logo/logo.jpg',
+};
+
+export default account;
